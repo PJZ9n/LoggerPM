7e94f9de31525c4337b77dec80abbb1ec760fa5a
PJZ9n <pjz9n@yahoo.co.jp>
2019-11-03 10:47:12 +0900

    [update] 処理を追加

--- src/PJZ9n/LoggerPM/Database/LoggerDatabase.php
+++ src/PJZ9n/LoggerPM/Database/LoggerDatabase.php
@@ -33,6 +33,8 @@
     namespace PJZ9n\LoggerPM\Database;
     
     use PJZ9n\LoggerPM\Library\Database\Type\SqliteDatabase;
+    use PJZ9n\LoggerPM\Log\LogActionType;
+    use SQLite3;
     
     /**
      * Class LoggerDatabase
@@ -55,6 +57,42 @@
                 "created_at NUMERIC NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime'))" .
                 ")"
             );
+        }
+        
+        /**
+         * アクションログに追加する
+         * @param string $playerName プレイヤー名
+         * @param string $actionType アクションタイプ
+         * @param array|null $actionData データ
+         * @param bool|null $actionCancelled キャンセルされたか
+         * @see LogActionType $actionTypeに使う
+         */
+        public function addActionLog(string $playerName, string $actionType, ?array $actionData = null, ?bool $actionCancelled = null): void
+        {
+            $playerName = SQLite3::escapeString($playerName);
+            $actionType = SQLite3::escapeString($actionType);
+            if ($actionData === null) {
+                $actionData = "null";
+            } else {
+                $actionData = "'" . SQLite3::escapeString(json_encode($actionData, JSON_UNESCAPED_UNICODE)) . "'";
+            }
+            if ($actionCancelled === null) {
+                $actionCancelled = "null";
+            } else {
+                if (!$actionCancelled) {
+                    $actionCancelled = "false";
+                } else {
+                    $actionCancelled = "true";
+                }
+            }
+            
+            $sql = "INSERT INTO action_logger(" .
+                "player_name, action_type, action_data, action_cancelled" .
+                ") VALUES (" .
+                "'{$playerName}', '{$actionType}', {$actionData}, {$actionCancelled}" .
+                ")";
+            
+            $this->getSqlite3()->exec($sql);
         }
         
     }

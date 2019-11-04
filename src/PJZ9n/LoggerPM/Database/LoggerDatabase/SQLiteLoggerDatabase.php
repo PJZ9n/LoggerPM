dabd52bee42dfe29610e1382af7b0b3029b38c5c
PJZ9n <pjz9n@yahoo.co.jp>
2019-11-04 10:27:57 +0900

    [update] SQLの処理を変更

--- src/PJZ9n/LoggerPM/Database/LoggerDatabase/SQLiteLoggerDatabase.php
+++ src/PJZ9n/LoggerPM/Database/LoggerDatabase/SQLiteLoggerDatabase.php
@@ -32,9 +32,7 @@
     
     namespace PJZ9n\LoggerPM\Database\LoggerDatabase;
 
-    use ErrorException;
     use PJZ9n\LoggerPM\Library\Database\Type\SqliteDatabase;
-    use SQLite3;
 
     /**
      * Class SQLiteLoggerDatabase
@@ -47,98 +45,52 @@
         {
             parent::__construct($filePath);
     
-            $this->getSqlite3()->exec(
-                "CREATE TABLE IF NOT EXISTS action_logger(" .
-                "id INTEGER NOT NULL PRIMARY KEY," .
-                "player_name TEXT NOT NULL," .
-                "action_type TEXT NOT NULL," .
-                "action_data JSON," .//データ JSON
-                "action_cancelled NUMERIC," .//キャンセルされたか 論理値
-                "created_at NUMERIC NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime'))" .
-                ")"
-            );
-        }
-    
-        public function addActionLog(string $playerName, string $actionType, ?array $actionData = null, ?bool $actionCancelled = null): void
-        {
-            $playerName = SQLite3::escapeString($playerName);
-            $actionType = SQLite3::escapeString($actionType);
-            if ($actionData === null) {
-                $actionData = "null";
-            } else {
-                $actionData = "'" . SQLite3::escapeString(json_encode($actionData, JSON_UNESCAPED_UNICODE)) . "'";
-                if (json_last_error() !== JSON_ERROR_NONE) {
-                    throw new ErrorException(json_last_error_msg());
-                }
-            }
-            if ($actionCancelled === null) {
-                $actionCancelled = "null";
-            } else {
-                if (!$actionCancelled) {
-                    $actionCancelled = "false";
-                } else {
-                    $actionCancelled = "true";
-                }
-            }
-    
-            $sql = "INSERT INTO action_logger(" .
-                "player_name, action_type, action_data, action_cancelled" .
-                ") VALUES (" .
-                "'{$playerName}', '{$actionType}', {$actionData}, {$actionCancelled}" .
-                ")";
+            $sql = <<< SQL
+                CREATE TABLE IF NOT EXISTS action_logger(
+                    id INTEGER NOT NULL PRIMARY KEY,
+                    player_name TEXT NOT NULL,
+                    action_type TEXT NOT NULL,
+                    action_data JSON,
+                    action_cancelled NUMERIC,
+                    created_at NUMERIC NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime'))
+                )
+            SQL;
     
             $this->getSqlite3()->exec($sql);
         }
     
+        public function addActionLog(string $playerName, string $actionType, ?array $actionData = null, ?bool $actionCancelled = null): void
+        {
+            $sql = <<< SQL
+                INSERT INTO action_logger(
+                    player_name,
+                    action_type,
+                    action_data,
+                    action_cancelled
+                ) VALUES (
+                    :player_name,
+                    :action_type,
+                    :action_data,
+                    :action_cancelled
+                )
+            SQL;
+    
+            $stmt = $this->getSqlite3()->prepare($sql);
+            $stmt->bindValue(":player_name", $playerName, SQLITE3_TEXT);
+            $stmt->bindValue(":action_type", $actionType, SQLITE3_TEXT);
+            $actionData === null ?
+                $stmt->bindValue(":action_data", null, SQLITE3_NULL) :
+                $stmt->bindValue(":action_data", json_encode($actionData), SQLITE3_TEXT);
+            $actionCancelled === null ?
+                $stmt->bindValue(":action_cancelled", null, SQLITE3_NULL) :
+                $stmt->bindValue(":action_cancelled", $actionCancelled, SQLITE3_INTEGER);
+            $stmt->execute();
+            $stmt->close();
+        }
+    
         public function getActionLogAll(?int $start = null, ?int $end = null, ?int $limit = null): array
         {
-            $sql = "SELECT * FROM action_logger";
-            $where = "";
-    
-            //TODO: もっと良い方法があるはず
-            if ($start !== null && $end !== null) {//両方指定
-                $start = date("Y-m-d H:i:s", $start);
-                $end = date("Y-m-d H:i:s", $end);
-                $where = " WHERE created_at >= '{$start}'" .
-                    " AND " .
-                    "created_at <= '{$end}'";
-            } else if ($start !== null && $end === null) {//startだけ指定
-                $start = date("Y-m-d H:i:s", $start);
-                $where = " WHERE created_at >= '{$start}'";
-            } else if ($start === null && $end !== null) {//endだけ指定
-                $end = date("Y-m-d H:i:s", $end);
-                $where = " WHERE created_at <= '{$end}'";
-            }
-    
-            $sql .= $where;
-    
-            $limitSql = "";
-    
-            if ($limit !== null) {
-                $limitSql = " LIMIT {$limit}";
-            }
-    
-            $sql .= $limitSql;
-    
-            echo "SQL: " . $sql . PHP_EOL;
-            
-            $result = $this->getSqlite3()->query($sql);
-    
-            $allActionLogs = [];
-    
-            while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-                if ($row["action_data"] !== null) {
-                    $row["action_data"] = json_decode($row["action_data"], true);
-                    //本当はjson_decodeが例外出してほしい！
-                    if (json_last_error() !== JSON_ERROR_NONE) {
-                        throw new ErrorException(json_last_error_msg());
-                    }
-                }
-                $row["created_at"] = strtotime($row["created_at"]);
-                $allActionLogs[] = $row;
-            }
-    
-            return $allActionLogs;
+            return [];
         }
     
         public function getActionLogByPlayerName(string $playerName, ?int $start = null, ?int $end = null, ?int $limit = null): array
